986aafaa084de6c1962d07db32b42741fa6fa59f
Bramtej <79318078+Bramtej@users.noreply.github.com>
2021-10-11 18:41:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+function gototelugubooks()
+{
+    window.location="telugu books.html";
+}
+
+function gotoenglishbooks()
+{
+    window.location = "english books.html";
+}
+
+function readteluguhanumanbook()
+{
+    window.location.href = "http://127.0.0.1:5500/telugu%20hanuman%20book.html";
+}
+
+function readteluguNaseeruddinbook()
+{
+    window.location.href = "http://127.0.0.1:5500/telugu%20nasseruddin%20book.html";
+}
+
+function readenglishhanumanbook()
+{
+    window.location.href = "http://127.0.0.1:5500/english%20hanuman%20book.html";
+}
+
+function readenglishnaseeruddinbook()
+{
+    window.location.href = "http://127.0.0.1:5500/english%20naseeruddin%20book.html";
+}
+
+function readenglishjatakabook()
+{
+    window.location.href = "http://127.0.0.1:5500/english%20jataka%20book.html";
+}
+
+function readtelugujatkabook()
+{
+    window.location.href = "http://127.0.0.1:5500/telugu%20jatakabook.html";
+}
+
+function back()
+{
+    window.location = "INDEX.HTML"
+}
+
+function eback1()
+{
+    window.location ="english books.html";
+}
+
+function eback2()
+{
+    window.location ="english books.html";
+}
+
+function eback3()
+{
+    window.location ="english books.html";
+}
+
+function tback1()
+{
+    window.location = "telugu books.html";
+}
+
+function tback2()
+{
+    window.location = "telugu books.html";
+}
+
+function tback3()
+{
+    window.location = "telugu books.html";
+}
+
+function readtelugualibaba()
+{
+    window.location = "telugu alibaba.html";
+}
+
+function readteluguAladdin()
+{
+    window.location = "telugu aladdin.html";
+}
+
+function readtelugukrishna()
+{
+    window.location = "telugu krishna.html";
+}
+
+function readenglishalibababook()
+{
+    window.location = "english alibaba.html";
+}
+
+function readenglishkrishnabook()
+{
+    window.location = "english krishna.html";
+}
+
+function readenglishaladdinbook()
+{
+    window.location = "english aladdin.html";
+}
